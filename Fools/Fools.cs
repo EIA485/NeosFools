18fb8a9ab09342324db3f516f1bdd27af707be81
eia485 <eia485git@gmail.com>
2021-09-21 23:42:19 -0700

    Add project files.

---
+++ Fools/Fools.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace Fools
+{
+	public class Fools : NeosMod
+	{
+		public override string Name => "Fools";
+		public override string Author => "eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/eia485/NeosFools/";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("net.eia485.Fools");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(Engine), "IsAprilFools", MethodType.Getter)]
+		class FoolsPatch
+		{ 
+			public static bool Prefix(ref bool __result) 
+			{
+				__result = true;
+				return false;
+			}
+		}
+	}
+}
